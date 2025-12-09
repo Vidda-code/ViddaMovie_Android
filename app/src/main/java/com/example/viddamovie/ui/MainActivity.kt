9ba490f5dd440aa9cad852c74db54a71dff66d28
David Godfrey <kinqvidda@gmail.com>
2025-12-09 14:50:03 +0100

    splash screen animation

--- app/src/main/java/com/example/viddamovie/ui/MainActivity.kt
+++ app/src/main/java/com/example/viddamovie/ui/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.viddamovie.ui
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.viddamovie.ui.navigation.AppNavigation
 import com.example.viddamovie.ui.theme.ViddaMovieTheme
@@ -14,9 +18,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
+        installSplashScreen().setOnExitAnimationListener { splashScreenView ->
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.view.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 200L
+
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            slideUp.start()
+        }
 
         enableEdgeToEdge()
         setContent {
